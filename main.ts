41a924276fd42269872ee72a184f79fc43881212
Deetya-2012 <deetyarenish@gmail.com>
2025-08-19 22:03:49 +0300

    I have done with the tech trails text book

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-
+game.splash("Enter a number ")
+let number = game.askForNumber("")
+let remain = number % 2
+if (remain == 0) {
+    game.splash("" + number + "is even number")
+} else {
+    game.splash("" + number + "is odd number")
+}
